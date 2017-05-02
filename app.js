6d5bbb07a641a9632cce95dc1b7b53d4c3c1ffa0
Matheus Azzi <matheuslazzi@gmail.com>
2017-05-02 20:54:28 -0300

    Init

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const index = require('./routes/index')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  res.status(err.status || 500)
+  res.json({error: err.message})
+})
+
+module.exports = app
