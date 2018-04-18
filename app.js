d7d7caa5684ef2dd697e033ae8cfc22ac7cf4edf
Matheus Azzi <matheuslazzi@gmail.com>
2018-04-18 17:08:40 -0300

    Add cors

--- app.js
+++ app.js
@@ -17,7 +17,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', routes)
 
-app.use('/graphql', graphQLHTTP({
+app.use('/graphql', (req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Headers', 'content-type, authorization, content-length, x-requested-with, accept, origin')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+  res.header('Allow', 'POST, GET, OPTIONS')
+  res.header('Access-Control-Allow-Origin', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200)
+  } else {
+    next()
+  }
+}, graphQLHTTP({
   schema,
   graphiql: { endpointURL: '/repl' }
 }))
